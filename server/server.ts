063343c76748fcf34ed9e0e6d0e418da05f5f39f
zachrosen <zach.rosen@yahoo.com>
2016-06-28 14:28:26 -0700

    html work

--- server/server.ts
+++ server/server.ts
@@ -16,7 +16,7 @@ var connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: '',
-  database: 'sampleDB'
+  database: 'TaxCalc'
 });
 
 connection.connect(function(error) {
