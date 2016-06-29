9a4d5e4485eec64013a3d9e6b871df817700fb9c
benstelzer <benstelzer@berkeley.edu>
2016-06-29 14:02:30 -0700

    update

--- server/api/tax/controller.ts
+++ server/api/tax/controller.ts
@@ -1,5 +1,6 @@
 import * as express from "express";
 
+
 export function multiply5 (req: express.Request, res: express.Response, next) {
   let details = {salary: req.body.salary * 5,
   filingType: req.body.filingType};
