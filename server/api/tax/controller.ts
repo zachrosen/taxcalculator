ecb483b89e0624a522a2eb5203ec76fe792db369
stevenfogel <stevenfogel@berkeley.edu>
2016-06-29 16:58:44 -0700

    nothing

--- server/api/tax/controller.ts
+++ server/api/tax/controller.ts
@@ -12,14 +12,22 @@ var connection = mysql.createConnection({
 export function multiply5 (req: express.Request, res: express.Response, next) {
 let filingType = req.body.filingType;
 let salary = req.body.salary;
+let taxOwed = 0;
 
   connection.query('SELECT `'+filingType+'` FROM `federal_tax`', function (error, results, fields) {
 
-  console.log(results[0][filingType]);
-
-  let details = {salary: salary}
-
+if(salary < results[0][filingType]) {
+  connection.query('SELECT `tax_rate` FROM `federal_tax`', function (error, results, fields) {
+    let details = {salary: salary * results[0].tax_rate}
+      res.json(details);
+  })
+} else if (results[0][filingType] < salary && salary < results[1][filingType]) {
+  let newSalary = salary - 9275
+  let details = {salary: 927.5 + (newSalary * 0.15)}
     res.json(details);
+}
+
+
   });
 
 }
