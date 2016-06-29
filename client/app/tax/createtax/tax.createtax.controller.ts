ab7572bd083ef2cd20d10821ad095d519a57f676
zachrosen <zach.rosen@yahoo.com>
2016-06-29 12:03:24 -0700

    added input features for deductions and credit

--- client/app/tax/createtax/tax.createtax.controller.ts
+++ client/app/tax/createtax/tax.createtax.controller.ts
@@ -1,13 +1,39 @@
 namespace app {
   export class TaxCreateTaxController {
     public tax: ITax;
+    public federalDeductionsTable= [];
+    public creditTable= [];
+    public stateDeductionsTable= [];
+
+    public addFederalDeduction() {
+      this.federalDeductionsTable.push({
+        federalDeductionDescription: '',
+        federalDeductionAmt: parseInt('')
+      });
+    }
+
+    public addCredit() {
+      this.creditTable.push({
+        creditDescription: '',
+        creditAmt: parseInt('')
+      });
+    }
+
+    public addStateDeduction() {
+      this.stateDeductionsTable.push({
+        stateDeductionDescription: '',
+        stateDeductionAmt: parseInt('')
+      })
+    }
+
     public createTax() {
       this.TaxService.createTax(this.tax).then(() => {
       this.$state.go('result');
     }, (err) => {
       alert(err);
     })
-    }
+  };
+
     constructor(
       private TaxService: app.TaxService,
       private $state: ng.ui.IStateService
