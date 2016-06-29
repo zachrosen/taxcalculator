5f2777255c66ba0451a1f27b33543855b141af67
stevenfogel <stevenfogel@berkeley.edu>
2016-06-29 14:54:48 -0700

    my attempted algo

--- server/api/tax/controller.ts
+++ server/api/tax/controller.ts
@@ -9,13 +9,38 @@ var connection = mysql.createConnection({
   database: 'taxDB'
 });
 
-
 export function multiply5 (req: express.Request, res: express.Response, next) {
   connection.query('SELECT `single_filers` FROM `federal_tax`', function (error, results, fields) {
 
-    let details = {salary: req.body.salary * results[0].single_filers,
-    filingType: req.body.filingType};
-    res.json(details);
+    if(req.body.salary < results[0].single_filers) {
+      let details = {salary: req.body.salary * 0.1,
+      filingType: req.body.filingType};
+      res.json(details);
+    } else if (results[0].single_filers < req.body.salary < results[1].single_filers) {
+      let details = {salary: req.body.salary * 0.15,
+      filingType: req.body.filingType};
+      res.json(details);
+    } else if (results[1].single_filers < req.body.salary < results[2].single_filers) {
+      let details = {salary: req.body.salary * 0.25,
+      filingType: req.body.filingType};
+      res.json(details);
+    } else if (results[2].single_filers < req.body.salary < results[3].single_filers) {
+      let details = {salary: req.body.salary * 0.28,
+      filingType: req.body.filingType};
+      res.json(details);
+    } else if (results[3].single_filers < req.body.salary < results[4].single_filers) {
+      let details = {salary: req.body.salary * 0.33,
+      filingType: req.body.filingType};
+      res.json(details);
+    } else if (results[4].single_filers < req.body.salary < results[5].single_filers) {
+      let details = {salary: req.body.salary * 0.35,
+      filingType: req.body.filingType};
+      res.json(details);
+    } else if (req.body.salary > results[6].single_filers) {
+      let details = {salary: req.body.salary * 0.396,
+      filingType: req.body.filingType};
+      res.json(details);
+    }
 
   });
 }
