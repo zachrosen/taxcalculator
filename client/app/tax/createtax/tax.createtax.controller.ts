0cbcb7552ab8ef058648494b14d41339711defc7
zachrosen <zach.rosen@yahoo.com>
2016-06-30 10:31:33 -0700

    progress bar

--- client/app/tax/createtax/tax.createtax.controller.ts
+++ client/app/tax/createtax/tax.createtax.controller.ts
@@ -5,6 +5,21 @@ namespace app {
     public creditTable= [];
     public stateDeductionsTable= [];
 
+    public move() {
+  var elem = document.getElementById("myBar");
+  var width = 0;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 100) {
+      clearInterval(id);
+    } else {
+      width = width + 10;
+      elem.style.width = width + '%';
+      document.getElementById("demo").innerHTML = width * 1  + '%';
+    }
+  }
+}
+
     public addFederalDeduction() {
       this.federalDeductionsTable.push({
         federalDeductionDescription: '',
