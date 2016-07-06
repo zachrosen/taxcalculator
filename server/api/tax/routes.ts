ca55b59dc9fc74507a0cfe5acdf0c42c9635f965
zachrosen <zach.rosen@yahoo.com>
2016-07-05 17:04:26 -0700

    merge errors

--- server/api/tax/routes.ts
+++ server/api/tax/routes.ts
@@ -5,7 +5,6 @@ import * as controller from "./controller";
 const router = express.Router();
 
 
-
 router.post("/", controller.preTaxIncome, controller.federalAdjustments, controller.FederalDeductions, controller.AGIBefore,  controller.totalExemptions, controller.AGIAfter, controller.federalTaxAmount, controller.federalCredits, controller.additonalFederalAmount, controller.taxableFICA, controller.stateDeductions, controller.adjustedIncomeState, controller.nonrefundableRentersCredit, controller.stateTaxAmount, controller.californiaSDI, controller.additonalStateAmount, controller.californiaTaxableMentalHealth, controller.stateExemptionCredits, controller.stateMiscCredits, controller.sendBack);
 
 
