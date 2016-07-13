f5a3f204be08864c23798ebad9f7c32dc0c65b9a
stevenfogel <stevenfogel@berkeley.edu>
2016-07-13 15:11:11 -0700

    added postinstall and changed server.ts

--- server/server.ts
+++ server/server.ts
@@ -13,10 +13,10 @@ let app = express();
 //require('./api/tax/model');
 var connection = mysql.createConnection({
   //properties
-  host: 'sql3.freesqldatabase.com',
-  user: 'sql3127728',
-  password: '4nqnXFwiSH',
-  database: 'sql3127728'
+  host: process.env.HOST_KEY,
+  user: process.env.USER_KEY,
+  password: process.env.PASSWORD_KEY,
+  database: process.env.DATABASE_KEY
 });
 
 connection.connect(function(error) {
